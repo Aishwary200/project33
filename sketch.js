bc70e39cab8b6b71e3b1549f5bac70901c792963
Aishwary200 <amritakeshri3059@gmail.com>
2020-11-16 23:36:19 +0530

    project34

--- sketch.js
+++ sketch.js
@@ -82,27 +82,33 @@ function draw() {
      
      divisions[k].display();
    }
-   if(turn>=5){
-     gamestate="end"
-   }
-   if(particles!==null){
+   
+   if(particles!==null && gamestate!=="end"){
       
     if(particles.body.position.y>760){
       if(particles.body.position.x<160 || particles.body.position.x>640){
         score=score+500
-        particles=null
+        
+        if(turn>=5 )gamestate="end"
       }
       if(particles.body.position.x>160 && particles.body.position.x<400 ){
         score=score+200
-        particles=null
+        
+        if(turn>=5 ) gamestate="end"
       }
       if(particles.body.position.x>400 && particles.body.position.x<640){
         score=score+100
-        particles=null
+        
+        if(turn>=5 ) gamestate="end"
       }
     }
   }
    particles.display();
+   if(gamestate === "end"){
+   textSize(100)
+   text("Game over",150,300)
+   
+   }
 }
 function mousePressed(){
   
